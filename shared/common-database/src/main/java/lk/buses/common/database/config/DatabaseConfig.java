d9fb3f083c85a2013c1a0b2d2bb3faabe8c959c8
gsayesh <39440853+gsayesh@users.noreply.github.com>
2025-06-01 17:16:02 +0530

    All services ready to start

--- shared/common-database/src/main/java/lk/buses/common/database/config/DatabaseConfig.java
+++ shared/common-database/src/main/java/lk/buses/common/database/config/DatabaseConfig.java
@@ -1,11 +1,15 @@
 package lk.buses.common.database.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * Common database configuration.
+ * Note: @EnableJpaAuditing should be added at the application level (in each microservice)
+ * to avoid duplicate bean definitions when this common module is imported.
+ */
 @Configuration
-@EnableJpaAuditing
 @EnableTransactionManagement
 public class DatabaseConfig {
+    // Common database configurations can be added here
 }
